9ceafe277b35894183c9525a248bd3b0fd275377
bugra cantepe <66384957+bugracntp@users.noreply.github.com>
2021-11-26 18:30:01 +0300

    inital commit

---
+++ bankaMatik/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace bankaMatik
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            hesap hesap1 = new hesap("gunay gül", 5000);
+            int secim=Convert.ToInt32(Console.ReadLine());
+            int miktar;
+            Console.WriteLine("seçiminizi yapınız: 1-para çek \n 2-para yatır \n 3-hesap sorgu ");
+            if (secim==1)
+            {
+                miktar= Convert.ToInt32(Console.ReadLine());
+                hesap1.paraCek(miktar);
+            }
+            else if (secim==2)
+            {
+                miktar = Convert.ToInt32(Console.ReadLine());
+                hesap1.paraYatır(miktar);
+            }
+            else if (secim == 3)
+            {
+                hesap1.hesapSorgu();
+            }
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ bankaMatik/hesap.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace bankaMatik
+{
+    class hesap
+    {
+        private string adSoyad;
+        private double bakiye;
+
+        public hesap(string adSoyad, double bakiye)
+        {
+            this.adSoyad = adSoyad;//constratur ile classdan gelen verinin biribirine karışmaması için classdan gelen veriyi this ile ayırt ediyoruz.
+            this.bakiye = bakiye;
+            Console.WriteLine("Hoş geldin "+adSoyad);
+        }
+
+        public void paraCek(int miktar)
+        {
+            if (miktar>bakiye)
+            {
+                Console.WriteLine("para çekemezsiniz.");
+            }
+            else
+            {
+                Console.WriteLine("bakiyeniz :" + bakiye);
+                bakiye = bakiye - miktar;
+                Console.WriteLine("çekim gerçekleşti yeni bakiyeniz :" + bakiye);
+            }
+        }
+
+        public void paraYatır(int miktar)
+        {
+            if (miktar>1500)
+            {
+                Console.WriteLine("günlük yatırma limitiniz 1500 tl ");
+            }
+            else
+            {
+                Console.WriteLine("bakiyeniz :" + bakiye);
+                bakiye = bakiye + miktar;
+                Console.WriteLine("yatırma gerçekleşti yeni bakiyeniz :" + bakiye);
+            }
+            
+        }
+
+        public void hesapSorgu()
+        {
+            Console.WriteLine("hesabın sahibi :" + adSoyad + "\n bakiyeniz: " + bakiye);
+        }
+    }
+}
